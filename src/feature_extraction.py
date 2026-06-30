3e299a170a92635e349d8a1073584a139a26f50e
Tian Liang Zheng <tianliangzheng.tlz@gmail.com>
2026-06-30 02:30:00 -0700

    add function skeletons

---
+++ src/feature_extraction.py
@@ -0,0 +1,11 @@
+def extract_hog(image):
+  # returns vector
+  pass
+
+def extract_color_histogram(image):
+  # returns vector
+  pass
+
+def combine_features(hog_vec, color_vec):
+  #concatenate
+  return hog_vec + color_vec
